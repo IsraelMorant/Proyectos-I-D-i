e6a4b437dd5d21ab9f2bbc067d38670b5c4d1565
Israel Morante <124182172+IsraelMorant@users.noreply.github.com>
2026-08-04 18:32:20 -0600

    Remove comment regarding .env
    Removed comment about ensuring environment variables are set.

--- Programa/src/supabaseClient.js
+++ Programa/src/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Asegúrate de tener estas variables en un archivo .env local
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
